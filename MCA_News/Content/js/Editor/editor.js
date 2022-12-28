929cb2d743ed067f13bdae7fb7608ab7c90aeb80
shivraj tiwaskar <shivrajtiwaskar7@gmail.com>
2022-12-28 23:39:31 +0530

    s

--- MCA_News/Content/js/Editor/editor.js
+++ MCA_News/Content/js/Editor/editor.js
@@ -1,1 +1,66 @@
-﻿
+﻿$(document).ready(function () {
+    $.post('Dist', function (data) {
+        d = JSON.parse(data);
+        d.Table.map((dist) => {
+            $('#District').append(` <option value="${dist.DistrictId}">${dist.District}</option>`);
+        })
+    });
+
+});
+
+const add = () => {
+
+    let distictid = $('#District').val();
+    let taluka = $('#Taluka').val();
+
+    $.ajax({
+        type: 'GET',
+        url: "AddNewTaluka", cache: false,
+        data: { Dist_ID: distictid, taluka: taluka },
+        success: (a) => {
+            Count_batch();
+        }
+        , error: (e) => {
+            console.log(e);
+        }
+    })
+
+}
+async function delete_data(obj) {
+    debugger
+    let id = obj.getAttribute('id');
+    id = id.split('_');
+    await $.post('Delete', { Id: id[1] }, function (data) {
+
+    });
+
+    location.reload();
+}
+const LinkButton = () => {
+    const href = document.getElementById('link').getAttribute('data-href');
+    window.location.href = href;
+}
+
+document.addEventListener('DOMContentLoaded', pageload, false)
+
+function pageload() {
+
+    debugger;
+    const c = (window.location.pathname).split('/')[2];
+    if (c == 'AddTaluka') { Count_batch(); }
+
+
+}
+const Count_batch = () => {
+    debugger;
+    let finaldata;
+    //let TalukaId = document.getElementById('TalukaId');
+    let batch = document.getElementById('batch');
+    $.post('NEWID', function (data) {
+        finaldata = JSON.parse(data);
+        finaldata = finaldata.Table[0];
+        batch.setAttribute("data-count", finaldata.count)
+        /*TalukaId.value = finaldata.id*/
+
+    });
+}
---
+++ MCA_News/Controllers/Admin/EditorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MCA_News.Controllers.Admin
+{
+    public class EditorController : Controller
+    {
+        // GET: Editor
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
